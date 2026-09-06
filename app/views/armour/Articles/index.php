0a60a049448c48e25a273c314f8d971d8d2e4b46
Dahelp <Dahelp@users.noreply.github.com>
2026-09-06 17:23:41 +0300

    Fix header spacing and article catalog layout

--- app/views/armour/Articles/index.php
+++ app/views/armour/Articles/index.php
@@ -15,32 +15,28 @@
 					<header class="page-header">
 						<h1 class="entry-title"><span><?=$type->name;?></span></h1>
 					</header>
-					<div class="flex flex-wrap blog">
+					<div class="articles-grid">
 						<?php foreach($conts as $item) { ?>
-							<div class="col-md-3 cont-one">
-								<div class="cont_ht border border-grey">
-									<div class="cont_blok_img">
+							<article class="article-card">
+								<a class="article-card__image" href="<?=htmlspecialchars($type->param_url . '/' . $item['alias'], ENT_QUOTES, 'UTF-8');?>">
 										<?php if($item["img"] !="") { ?>
-											<img src="images/contents/mini/<?=$item["img"]?>" alt="<?=$item["name"]?>" title="<?=$item["name"]?>" style="width:100%" />
+											<img src="<?=PATH;?>/images/contents/mini/<?=htmlspecialchars($item['img'], ENT_QUOTES, 'UTF-8');?>" alt="<?=htmlspecialchars($item['name'], ENT_QUOTES, 'UTF-8');?>" loading="lazy" />
 										<?php } else { ?>
-											<img src="images/no_image.jpg" alt="" />
+											<img src="<?=PATH;?>/images/no_image.jpg" alt="" loading="lazy" />
 										<?php } ?>
-									</div>
-									<div class="cont_info">
+								</a>
+								<div class="article-card__body">
 										<?php if($type["hide_date_post"] == "show") { ?>
-											<div class="cont_info_data">
+											<time class="article-card__date" datetime="<?=date('Y-m-d', strtotime($item['date_post']));?>">
 												<?php echo \ishop\App::contdate($item["date_post"]); ?>
-											</div>
+											</time>
 										<?php } ?>
-										<div class="cont_info_name">
-											<a href="<?=$type->param_url;?>/<?=$item["alias"];?>"><?=$item["name"];?></a>
-										</div>
-										<div class="cont_info_anons">
-											<?php echo mb_strimwidth($item["anons"], 0, 200, "...");?>
-										</div>
+										<h2 class="article-card__title"><a href="<?=htmlspecialchars($type->param_url . '/' . $item['alias'], ENT_QUOTES, 'UTF-8');?>"><?=htmlspecialchars($item['name'], ENT_QUOTES, 'UTF-8');?></a></h2>
+										<?php if (trim((string) $item['anons']) !== '') { ?>
+											<p class="article-card__excerpt"><?=htmlspecialchars(mb_strimwidth(strip_tags($item['anons']), 0, 180, '…'), ENT_QUOTES, 'UTF-8');?></p>
+										<?php } ?>
 									</div>
-								</div>
-							</div>
+							</article>
 						<?php } ?>
 					</div>
 					<div class="blog-nav">
