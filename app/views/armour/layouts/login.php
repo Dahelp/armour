86af0d510eb24ce4c5fb15f677f980d8a32efc3e
Dahelp <Dahelp@users.noreply.github.com>
2026-09-16 14:29:55 +0300

    Remove admin login preload blockers

--- app/views/armour/layouts/login.php
+++ app/views/armour/layouts/login.php
@@ -7,22 +7,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Вход в панель управления</title>
     <link rel="shortcut icon" href="<?=PATH;?>/images/star.png" type="image/png" />
-    <!-- Tempusdominus Bootstrap 4 -->
-    <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Theme style -->
     <link rel="stylesheet" href="dist/css/adminlte.min.css">
-    <!-- iCheck -->
-    <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
 	<!-- bypstyle-->
     <link rel="stylesheet" href="my.css">
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 </head>
-<body class="hold-transition login-page">
+<body class="login-page">
 
 <?=$content;?>
 
